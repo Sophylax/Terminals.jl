d7f9af659d0cc685b324c6adf9681092c01adf53
Ulaş Sert <Sophylax@users.noreply.github.com>
2018-10-26 15:17:15 +0300

    Fix immediate LoadErrors in Terminal.jl

--- src/Terminals.jl
+++ src/Terminals.jl
@@ -1,17 +1,17 @@
 module Terminals
     import Base.size, Base.write, Base.flush
-    abstract TextTerminal <: Base.IO
+    abstract type TextTerminal <: Base.IO end
     export TextTerminal, NCurses, writepos, cmove, pos, getX, getY, hascolor
 
     # Stuff that really should be in a Geometry package
-    immutable Rect
+    struct Rect
         top
         left
         width
         height
     end
 
-    immutable Size
+    struct Size
         width
         height
     end 
@@ -19,7 +19,7 @@ module Terminals
 
     # INTERFACE
     size(::TextTerminal) = error("Unimplemented")
-    writepos(t::TextTerminal,x,y,s::Array{Uint8,1}) = error("Unimplemented")
+    writepos(t::TextTerminal,x,y,s::Array{UInt8,1}) = error("Unimplemented")
     cmove(t::TextTerminal,x,y) = error("Unimplemented")
     getX(t::TextTerminal) = error("Unimplemented")
     getY(t::TextTerminal) = error("Unimplemented")
@@ -52,7 +52,7 @@ module Terminals
     hascolor(::TextTerminal) = false
 
     # Utility Functions
-    function writepos{T}(t::TextTerminal, x, y, b::Array{T})
+    function writepos(t::TextTerminal, x, y, b::Array{T}) where T
         if isbits(T)
             writepos(t,x,y,reinterpret(Uint8,b))
         else
@@ -78,7 +78,7 @@ module Terminals
 
     beep(t::TextTerminal) = nothing
 
-    abstract TextAttribute
+    abstract type TextAttribute end
 
     module Attributes
         # This is just to get started and will have to be revised
@@ -88,31 +88,24 @@ module Terminals
         export Standout, Underline, Reverse, Blink, Dim, Bold, AltCharset, Invisible, Protect, Left, Right, Top,
                 Vertical, Horizontal, Low
 
-        macro flag_attribute(name)
-            quote
-                immutable $name <: TextAttribute
-                end
-            end
-        end
-
-        @flag_attribute Standout
-        @flag_attribute Underline
-        @flag_attribute Reverse
-        @flag_attribute Blink
-        @flag_attribute Dim
-        @flag_attribute Bold
-        @flag_attribute AltCharset
-        @flag_attribute Invisible
-        @flag_attribute Protect
-        @flag_attribute Left
-        @flag_attribute Right
-        @flag_attribute Top
-        @flag_attribute Vertical
-        @flag_attribute Horizontal
-        @flag_attribute Low
+        struct Standout <: TextAttribute end
+        struct Underline <: TextAttribute end
+        struct Reverse <: TextAttribute end
+        struct Blink <: TextAttribute end
+        struct Dim <: TextAttribute end
+        struct Bold <: TextAttribute end
+        struct AltCharset <: TextAttribute end
+        struct Invisible <: TextAttribute end
+        struct Protect <: TextAttribute end
+        struct Left <: TextAttribute end
+        struct Right <: TextAttribute end
+        struct Top <: TextAttribute end
+        struct Vertical <: TextAttribute end
+        struct Horizontal <: TextAttribute end
+        struct Low <: TextAttribute end
 
         attr_simplify(::TextTerminal, x::TextAttribute) = x
-        attr_simplify{T<:TextAttribute}(::TextTerminal, ::Type{T}) = T()
+        attr_simplify(::TextTerminal, ::typeof(T)) where T <: TextAttribute = T()
         function attr_simplify(::TextTerminal, s::Symbol)
             if s == :standout 
                 return Standout()
@@ -128,29 +121,29 @@ module Terminals
 
     end
 
-    module Colors 
+    module TerminalColorWrap 
         import Terminals.TextAttribute, Terminals.TextTerminal, Terminals.Attributes.attr_simplify
-        using Color
+        using Colors
 
         export TerminalColor, TextColor, BackgroundColor, ForegroundColor, approximate,
                 lookup_color, terminal_color, maxcolors, maxcolorpairs, palette, numcolors
 
         # Represents a color actually displayable by the current terminal
-        abstract TerminalColor
+        abstract type TerminalColor end
 
-        immutable TextColor <: TextAttribute
+        struct TextColor <: TextAttribute
             c::TerminalColor
         end
-        immutable BackgroundColor <: TextAttribute
+        struct BackgroundColor <: TextAttribute
             c::TerminalColor
         end
 
-        function approximate(t::TextTerminal, c::ColorValue)
+        function approximate(t::TextTerminal, c::Color)
             x = keys(palette(t))
             lookup_color(t,x[indmin(map(x->colordiff(c,x),x))])
         end
 
-        attr_simplify(t::TextTerminal, c::ColorValue) = TextColor(lookup_color(t,c))
+        attr_simplify(t::TextTerminal, c::Color) = TextColor(lookup_color(t,c))
 
         # Terminals should implement this
         lookup_color(t::TextTerminal) = error("Unimplemented")
@@ -161,15 +154,15 @@ module Terminals
     end
 
     module Unix
-        importall Terminals
+        #importall Terminals
 
         import Terminals: width, height, cmove, Rect, Size, getX, 
-                          getY, raw!, clear, clear_line, beep, hascolor
-        import Base: size, read, write, flush, TTY, writemime, readuntil, start_reading, stop_reading
+                          getY, raw!, clear, clear_line, beep, hascolor, TextTerminal
+        import Base: size, read, write, flush, TTY, readuntil, start_reading, stop_reading
 
         export UnixTerminal
 
-        type UnixTerminal <: TextTerminal
+        mutable struct UnixTerminal <: TextTerminal
             term_type
             in_stream::TTY
             out_stream::TTY
@@ -186,13 +179,13 @@ module Terminals
         cmove_line_down(t::UnixTerminal,n) = (cmove_down(t,n);cmove_col(t,0))
         cmove_col(t::UnixTerminal,n) = write(t.out_stream,"$(CSI)$(n)G")
 
-        raw!(t::UnixTerminal,raw::Bool) = ccall(:uv_tty_set_mode,Int32,(Ptr{Void},Int32),t.in_stream.handle,raw?1:0)!=-1
+        raw!(t::UnixTerminal,raw::Bool) = ccall(:uv_tty_set_mode,Int32,(Ptr{Nothing},Int32),t.in_stream.handle,raw ? 1 : 0)!=-1
         enable_bracketed_paste(t::UnixTerminal) = write(t.out_stream,"$(CSI)?2004h")
         disable_bracketed_paste(t::UnixTerminal) = write(t.out_stream,"$(CSI)?2004l")
 
         function size(t::UnixTerminal)
             s = Array(Int32,2)
-            Base.uv_error("size (TTY)",ccall(:uv_tty_get_winsize,Int32,(Ptr{Void},Ptr{Int32},Ptr{Int32}),t.out_stream.handle,pointer(s,1),pointer(s,2))!=0)
+            Base.uv_error("size (TTY)",ccall(:uv_tty_get_winsize,Int32,(Ptr{Nothing},Ptr{Int32},Ptr{Int32}),t.out_stream.handle,pointer(s,1),pointer(s,2))!=0)
             Size(s[1],s[2])
         end
 
@@ -200,22 +193,23 @@ module Terminals
         clear_line(t::UnixTerminal) = write(t.out_stream,"\x1b[0G\x1b[0K")
         beep(t::UnixTerminal) = write(t.err_stream,"\x7")
 
-        write{T,N}(t::UnixTerminal,a::Array{T,N}) = write(t.out_stream,a)
-        write(t::UnixTerminal,p::Ptr{Uint8}) = write(t.out_stream,p)
-        write(t::UnixTerminal,p::Ptr{Uint8},x::Integer) = write(t.out_stream,p,x)
-        write(t::UnixTerminal,x::Uint8) = write(t.out_stream,x)
-        read{T,N}(t::UnixTerminal,x::Array{T,N}) = read(t.in_stream,x)
+        write(t::UnixTerminal,a::Array{T,N}) where {T,N} = write(t.out_stream,a)
+        write(t::UnixTerminal,p::Ptr{UInt8}) = write(t.out_stream,p)
+        write(t::UnixTerminal,p::Ptr{UInt8},x::Integer) = write(t.out_stream,p,x)
+        write(t::UnixTerminal,x::UInt8) = write(t.out_stream,x)
+        read(t::UnixTerminal,x::Array{T,N}) where {T,N} = read(t.in_stream,x)
         readuntil(t::UnixTerminal,s::String) = readuntil(t.in_stream,s)
         readuntil(t::UnixTerminal,c::Char) = readuntil(t.in_stream,c) 
         readuntil(t::UnixTerminal,s) = readuntil(t.in_stream,s)
-        read(t::UnixTerminal,::Type{Uint8}) = read(t.in_stream,Uint8)
+        read(t::UnixTerminal,::Type{UInt8}) = read(t.in_stream,Uint8)
         start_reading(t::UnixTerminal) = start_reading(t.in_stream)
         stop_reading(t::UnixTerminal) = stop_reading(t.in_stream)
 
 
-        hascolor(t::UnixTerminal) = (beginswith(t.term_type,"xterm") || success(`tput setaf 0`))
+        hascolor(t::UnixTerminal) = (beginswith(t.term_type,"xterm") || success("tput setaf 0"))
         #writemime(t::UnixTerminal, ::MIME"text/plain", x) = writemime(t.out_stream, MIME("text/plain"), x)
     end
-    importall .Unix
+    #importall .Unix
     export UnixTerminal
+    
 end
